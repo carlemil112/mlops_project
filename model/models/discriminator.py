74b035fd471fe7b4076bfa5af0308675c2e576e3
rasmusmolby <rallemolby@gmail.com>
2026-02-04 11:11:32 +0100

    Initial model push. Patch gan model from miniproject on third semester.

---
+++ model/models/discriminator.py
@@ -0,0 +1,101 @@
+import torch.nn as nn
+import torch
+
+
+
+# PatchGan discriminator
+class Discriminator(nn.Module):
+    def __init__(
+                self, 
+                input_channels = 4, # 1 for grayscale 3 for RGB
+                stride = 2,
+                padding = 1,
+                kernel_size = 4,
+                relu_slope = 0.2
+            ):
+        
+        super().__init__()
+
+        self.layers = [64, 128, 256, 512] # layer sizes
+
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                input_channels,
+                self.layers[0],
+                kernel_size,
+                stride,
+                padding
+            ),
+            nn.LeakyReLU(relu_slope)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                self.layers[0],
+                self.layers[1],
+                kernel_size,
+                stride,
+                padding
+            ),
+            nn.BatchNorm2d(self.layers[1]),
+            nn.LeakyReLU(relu_slope)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(
+                self.layers[1],
+                self.layers[2],
+                kernel_size,
+                stride,
+                padding
+            ),
+            nn.BatchNorm2d(self.layers[2]),
+            nn.LeakyReLU(relu_slope)
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(
+                self.layers[2],
+                self.layers[3],
+                kernel_size,
+                1, # PatchGan changes stride to 1 so spartial map survives
+                padding
+            ),
+            nn.BatchNorm2d(self.layers[3]),
+            nn.LeakyReLU(relu_slope)
+        )
+        self.out_conv = nn.Sequential(nn.Conv2d(self.layers[3], 1, kernel_size, 1, padding),
+        nn.Sigmoid()
+        )
+
+
+# NOT USED IN PATCHGAN!!
+        # Get spartial size
+        # final_spartial = img_size // (stride**4)
+        # 512/(2⁴)=16, which means 16x16 is the final spartial size (height, width)
+        # self.flattened = self.layers[3] * final_spartial * final_spartial
+        # self.fc = nn.Linear(self.flattened, 1)
+
+
+    def forward(self, x):
+       # concatenated = torch.cat([real_data, fake_data])
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        logit_map = self.out_conv(x)
+        return logit_map
+
+
+
+
+
+# Sanity checkkkk
+
+if __name__=="__main__":
+
+    D = Discriminator(input_channels=4)
+    x = torch.randn(8,4,256,256)
+    out = D(x)
+    print(out.shape)
+
+
---
+++ model/unet_random_train.py
@@ -0,0 +1,168 @@
+import torch
+from torch import nn
+from torch import optim
+from torchvision import datasets
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from models.unet import Unet as unet
+from data_loader import gray_color_data
+import os
+
+import wandb
+
+
+def create_checkpoint(epoch, model, optimizer, loss, val_loss, run_id, path):
+    """ creating a checkpoint """
+    torch.save(
+        {'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        "loss": loss,
+        "val_loss": val_loss,
+        "run_id": run_id,
+        }, 
+        path
+    )
+
+
+def load_checkpoint(path, model, optimizer):
+    """ loading a checkpoint """
+    checkpoint = torch.load(path, weights_only=False)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+    epoch = checkpoint["epoch"]
+    loss = checkpoint["loss"]
+    val_loss = checkpoint["val_loss"]
+    run_id = checkpoint["run_id"]
+
+    return model, optimizer, epoch, loss, val_loss, run_id
+
+
+def train(model, device, optimizer, train_loader, epoch):
+    """ training loop """
+    model.train()
+    loss_function = nn.MSELoss()
+
+    for i, (x,y) in enumerate(train_loader):
+        x = x.to(device)
+        y = y.to(device)
+
+        optimizer.zero_grad()
+
+        y_pred = model(x)
+        loss = loss_function(y, y_pred)
+        loss.backward()
+        optimizer.step()
+
+        if i % 200 == 0:
+            print("Train Epoch: %s , Iteration: %s , Train Loss: %s" % (epoch, i, loss.item()))
+            wandb.log({"Train loss": loss.item()})
+    return loss.item
+
+
+def val(model, device, val_loader):
+    """ validation loop """
+    model.eval()
+    lossfunction = nn.L1Loss()
+    total_loss = 0
+    total_batch_size = 0
+
+    with torch.no_grad():
+        loss = 0
+        for batch, (x,y) in enumerate(val_loader):
+            x = x.to(device)
+            y = y.to(device)
+
+            output = model(x)
+            batch_loss = lossfunction(y, output).item()
+            batch_size = x.size(0)
+
+            total_loss += batch_loss * batch_size
+            total_batch_size += batch_size
+        
+        loss = total_loss / total_batch_size
+        print("\nVal MAE: ", loss, "\n")
+        wandb.log({"Val mae": loss})
+    return total_loss
+
+
+def save_test_image(pred_tensor, target_tensor, epoch):
+    pred_img = transforms.functional.to_pil_image(pred_tensor[0].detach().cpu())
+    pred_img.save(f"test_output_images/random_pred_img{epoch}.jpg")
+    target_img = transforms.functional.to_pil_image(target_tensor[0].detach().cpu())
+    target_img.save(f"test_output_images/random_target_img{epoch}.jpg")
+
+
+def main():
+    # setting up variables
+    number_epochs = 100
+    batch_size = 512
+    learning_rate = 1e-3
+    torch.manual_seed(42) #secure reproducibility
+    epoch = 1
+    loss = 0
+    val_loss = 100000000
+
+    # start up wandb
+    wandb.login()
+
+    # device
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        print("cuda")
+    else:
+        device = torch.device("cpu")
+        print("cpu")
+
+    # download of data
+    data_path = "data"
+    checkpoint_path = "checkpoint/random_unet.tar"
+
+    # download data
+    train_set = gray_color_data(data_path, split="train-standard", train=True)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=16)
+
+    val_set = gray_color_data(data_path, split="val", train=False)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=1, shuffle=False, num_workers=16)
+
+    # initilizing model
+    if torch.cuda.device_count() > 1:
+        model = nn.DataParallel(unet())
+        print(f"Using {torch.cuda.device_count()} GPU'S")
+    else:
+        model = unet()
+        print(f"Using {torch.cuda.device_count()} GPU'S")
+    model = model.to(device)
+
+    # define optimizers
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=0.0001)
+
+    if os.path.exists(checkpoint_path):
+        model, optimizer, epoch, loss, val_loss, run_id = load_checkpoint(checkpoint_path, model, optimizer)
+        run = wandb.init(project="unet_places_cropped", id = run_id, resume="must")
+        epoch += 1
+        print("Checkpoints is loaded")
+    else:
+        run = wandb.init(project="unet_places_cropped", name="cropped-image")
+        run_id = run.id
+
+    for epoch in range(epoch,number_epochs):
+        train_loss = train(model, device, optimizer, train_loader, epoch)
+        new_val_loss = val(model, device, val_loader)
+
+        if new_val_loss < val_loss:
+            print("Lower val result")
+            create_checkpoint(epoch, model, optimizer, train_loss, new_val_loss, run_id, checkpoint_path)
+            print("New checkpoint has been made")
+        
+        # saving one image to check progress
+        if epoch % 10 == 0:
+            for x, y in val_loader:
+                test_img = x.to(device)
+                model.eval()
+                test_pred = model(test_img)
+                save_test_image(test_pred, y, epoch)
+                break
+
+if __name__ == "__main__":
+    main()
