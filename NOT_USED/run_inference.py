fa1a0b854d22dc9db51c687df3bc24df0086a8ba
rasmusmolby <rallemolby@gmail.com>
2026-06-06 10:11:11 +0200

    Changed directory of c++ related script as not used anymore

--- NOT_USED/run_inference.py
+++ NOT_USED/run_inference.py
@@ -1,17 +1,13 @@
 from carbontracker.tracker import CarbonTracker
 import subprocess
 
-# FOR TRACKING INFERENCE CARBON FOOTPRINT, AS CARBONTRACKER IS NATIVELY PYTHON 
+# FOR TRACKING INFERENCE CARBON FOOTPRINT, AS CARBONTRACKER IS NATIVELY PYTHON
 # AND DOES NOT SUPPORT C++ DIRECTLY!!
 tracker = CarbonTracker(epochs=1)
 tracker.epoch_start()
 
 # Call the compiled C++ inference script
-result = subprocess.run(
-    ["./inference"],
-    capture_output=True,
-    text=True
-)
+result = subprocess.run(["./inference"], capture_output=True, text=True)
 
 # Print output from the C++ script
 print(result.stdout)
